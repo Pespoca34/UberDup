b022ec94152adb4f947cd61a26cd7653dcfd553e
Pedro Juliao <145300572+Pespoca34@users.noreply.github.com>
2026-09-22 23:48:28 +0100

    Feature | Project Skeleton

---
+++ backend/src/main/java/uberDup/UberDupApplication.java
@@ -0,0 +1,13 @@
+package uberDup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberDupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberDupApplication.class, args);
+	}
+
+}
